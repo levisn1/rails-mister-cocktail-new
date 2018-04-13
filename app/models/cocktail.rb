f8a981c4816b15527e6b999b2f92468e253857b6
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-04-13 12:31:59 +0200

    primo commit

---
+++ app/models/cocktail.rb
@@ -0,0 +1,8 @@
+class Cocktail < ApplicationRecord
+  has_many :ingredients, through: :doses
+  has_many :doses, dependent: :destroy
+
+
+
+  validates :name, presence: true, allow_blank: false, uniqueness: true
+end
